5d5258225e908eeb3d23a2f67d4f3cb80e9e53a3
Marcin Obrzut <obrzut.marcin@gmail.com>
2017-10-08 12:39:23 +0200

    Navigation | Adding search bar

--- js/main.js
+++ js/main.js
@@ -1,37 +1,48 @@
 'use strict';
 
-document.addEventListener("DOMContentLoaded",function(){
+document.addEventListener("DOMContentLoaded", function () {
     addEventListeners();
 
 });
 
-function addEventListeners(){
+/* Navigation */ 
+
+function addEventListeners() {
     var navItem = document.getElementById("main-nav-ul");
-    navItem.addEventListener('click',activateItem);
+    navItem.addEventListener('click', activateItem);
 }
 
-function activateItem(event){
+function activateItem(event) {
 
-   var arrayOfNavItem = document.getElementById(event.target.id)
+    var arrayOfNavItem = document.getElementById(event.target.id)
         .parentNode
         .parentNode
         .children;
 
-    for ( var i in arrayOfNavItem){ 
-        if(arrayOfNavItem[i]
-                .classList
-                    ){
-            arrayOfNavItem[i]
-                .classList
-                .remove('active');
+    if (event.target.id === 'search') {
+        if (document.getElementById('search-form-off')) {
+            document.getElementById('search-form-off')
+                .id = 'search-form-on';
         }
-    
-    }
 
-    document.getElementById(event.target.id)
-        .parentNode
-        .classList
-        .add('active');
+
+    }
+    if (event.target.id === 'cross') {
+        document.getElementById('search-form-on').id = 'search-form-off';
+    } else {
+        for (var i in arrayOfNavItem) {
+            if (arrayOfNavItem[i].classList) {
+                arrayOfNavItem[i]
+                    .classList
+                    .remove('active');
+            }
+
+        }
+
+        document.getElementById(event.target.id)
+            .parentNode
+            .classList
+            .add('active');
+    }
 }
- 
 
